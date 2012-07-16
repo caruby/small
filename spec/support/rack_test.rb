312d24920df9f56552d5a6f9989dbb057febfb4c
Fred Loney <loneyf@ohsu.edu>
2012-07-16 15:52:42 -0700

    Add context.

--- spec/support/rack_test.rb
+++ spec/support/rack_test.rb
@@ -2,7 +2,7 @@ require 'rack/test'
 require Bundler.environment.specs.detect { |s| s.name == 'caruby-tissue' }.full_gem_path + '/test/helpers/seed'
 
 def app
-  Sinatra::Application
+  CaSmall::App.new(CaTissue)
 end
 
 module CaSmall

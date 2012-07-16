703d9d0e0bf3e5e8c8ce3580ef25d3c409f51c13
Fred Loney <loneyf@ohsu.edu>
2012-07-16 15:51:59 -0700

    Reorder cases.

--- spec/find_spec.rb
+++ spec/find_spec.rb
@@ -1,19 +1,9 @@
 require 'uri'
 require 'spec/spec_helper'
-require 'jinx/helpers/uom'
 
 module CaSmall
   describe 'Find' do
     include RackTest
-
-  	it "should find an existing Participant" do
-      pnt = CaTissue::Participant.new(:last_name => Jinx::UOM.generate.to_s).create
-      get "/participant/#{pnt.id}"
-      last_response.should be_ok
-      fetched = JSON[last_response.body]
-      fetched.identifier.should_not be_nil
-      pnt.content_matches?(fetched).should be true
-    end
 
   	it "should find an existing Site by name" do
       site = CaTissue::Site.default_site
@@ -22,6 +12,15 @@ module CaSmall
       fetched = JSON[last_response.body]
       fetched.identifier.should_not be_nil
       site.content_matches?(fetched).should be true
+    end
+
+  	it "should find an existing Participant" do
+      pnt = CaTissue::Participant.new(:last_name => Jinx::UID.generate.to_s).create
+      get "/participant/#{pnt.id}"
+      last_response.should be_ok
+      fetched = JSON[last_response.body]
+      fetched.identifier.should_not be_nil
+      pnt.content_matches?(fetched).should be true
     end
   end
 end
